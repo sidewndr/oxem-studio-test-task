028225137f69d4d87fb14da73477adf4a69413ba
sidewndr <anoshkinzz@mail.ru>
2022-10-02 13:18:03 +0300

    add: loader

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import './Loader.scss'
+import {ReactComponent as LoaderIcon} from '../../attachments/icons/loader.svg'
+
+export const Loader = () => {
+  return (
+    <div className={'loader'}>
+      <LoaderIcon />
+    </div>
+  )
+}
